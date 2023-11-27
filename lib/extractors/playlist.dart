e54554601087e076421d290b78ea9ef058aa5ab3
MSOB7YY <mohamedsobhy201566@gmail.com>
2023-11-27 20:25:37 +0200

    fix: type cast

--- lib/extractors/playlist.dart
+++ lib/extractors/playlist.dart
@@ -8,12 +8,13 @@ class PlaylistExtractor {
   PlaylistExtractor._internal();
 
   /// Extract all the details of the given Playlist URL into a YoutubePlaylist object
-  Future<YoutubePlaylist> getPlaylistDetails(String? playlistUrl) async {
+  Future<YoutubePlaylist?> getPlaylistDetails(String? playlistUrl) async {
     StringChecker.ensureGoodLink(playlistUrl);
 
-    final info = await NewPipeExtractorDart.safeExecute(
-        "getPlaylistDetails", {"playlistUrl": playlistUrl});
-    return YoutubePlaylist.fromMap(info);
+    final info = await NewPipeExtractorDart.safeExecute("getPlaylistDetails", {"playlistUrl": playlistUrl});
+    if (info == null) return null;
+    final map = (info as Map<Object?, Object?>).cast<String, dynamic>();
+    return YoutubePlaylist.fromMap(map);
   }
 
   /// Extract all the Streams from the given Playlist URL
@@ -21,17 +22,14 @@ class PlaylistExtractor {
   Future<List<StreamInfoItem>> getPlaylistStreams(String? playlistUrl) async {
     StringChecker.ensureGoodLink(playlistUrl);
 
-    final info = await NewPipeExtractorDart.safeExecute(
-        "getPlaylistStreams", {"playlistUrl": playlistUrl});
+    final info = await NewPipeExtractorDart.safeExecute("getPlaylistStreams", {"playlistUrl": playlistUrl});
     return StreamsParser.parseStreamListFromMap(info);
   }
 
-  Future<List<StreamInfoItem>> getPlaylistStreamsNextPage(
-      String? playlistUrl) async {
+  Future<List<StreamInfoItem>> getPlaylistStreamsNextPage(String? playlistUrl) async {
     StringChecker.ensureGoodLink(playlistUrl);
 
-    final info = await NewPipeExtractorDart.safeExecute(
-        "getPlaylistStreamsNextPage", {"playlistUrl": playlistUrl});
+    final info = await NewPipeExtractorDart.safeExecute("getPlaylistStreamsNextPage", {"playlistUrl": playlistUrl});
     return StreamsParser.parseStreamListFromMap(info);
   }
 }
