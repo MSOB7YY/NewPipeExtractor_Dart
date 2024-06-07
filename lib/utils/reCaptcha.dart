c4cf283f12897a2352f85ebec8bfb214a6e4fd13
MSOB7YY <mohamedsobhy201566@gmail.com>
2024-06-07 23:42:53 +0300

    build: upgrade deps

--- lib/utils/reCaptcha.dart
+++ lib/utils/reCaptcha.dart
@@ -1,18 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:newpipeextractor_dart/newpipeextractor_dart.dart';
 import 'package:newpipeextractor_dart/utils/httpClient.dart';
 import 'package:newpipeextractor_dart/utils/navigationService.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 bool _resolvingCaptcha = false;
 
 class ReCaptchaPage extends StatefulWidget {
   const ReCaptchaPage({super.key});
 
-  static Future<dynamic> checkInfo(
-      info, Future<dynamic> Function() task) async {
+  static Future<dynamic> checkInfo(info, Future<dynamic> Function() task) async {
     if (info == null) return [];
     if ((info as Map).containsKey("error")) {
       if (info["error"].contains("reCaptcha")) {
@@ -31,25 +30,35 @@ class ReCaptchaPage extends StatefulWidget {
   }
 
   @override
-  _ReCaptchaPageState createState() => _ReCaptchaPageState();
+  State<ReCaptchaPage> createState() => _ReCaptchaPageState();
 }
 
 class _ReCaptchaPageState extends State<ReCaptchaPage> {
-  InAppWebViewController? controller;
+  late final controller = WebViewController();
+  String? initialUrl;
   String foundCookies = "";
 
   @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        final String url = ModalRoute.of(context)!.settings.arguments as String;
+        initialUrl = url;
+        controller.loadRequest(Uri.parse(url), headers: ExtractorHttpClient.defaultHeaders);
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final String url = ModalRoute.of(context)!.settings.arguments as String;
     return Material(
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
           title: ListTile(
-            title:
-                const Text("reCaptcha", style: TextStyle(color: Colors.white)),
-            subtitle: Text("Solve the reCaptcha and confirm",
-                style: TextStyle(color: Colors.white.withOpacity(0.6))),
+            title: const Text("reCaptcha", style: TextStyle(color: Colors.white)),
+            subtitle: Text("Solve the reCaptcha and confirm", style: TextStyle(color: Colors.white.withOpacity(0.6))),
           ),
           backgroundColor: Colors.redAccent,
           actions: [
@@ -57,38 +66,29 @@ class _ReCaptchaPageState extends State<ReCaptchaPage> {
               icon: const Icon(Icons.check_rounded),
               color: Colors.white,
               onPressed: () async {
-                final String currentUrl =
-                    await (controller?.getUrl() as FutureOr<String?>) ?? url;
-                final info = await NewPipeExtractorDart.execute(
-                    'getCookieByUrl', {"url": currentUrl});
-                String? cookies = info['cookie'];
+                final String? currentUrl = await (controller.currentUrl() as FutureOr<String?>) ?? initialUrl;
+                if (currentUrl == null) return;
+                final info = await NewPipeExtractorDart.execute('getCookieByUrl', {"url": currentUrl});
+                final String? cookies = info['cookie'];
                 handleCookies(cookies);
                 // Sometimes cookies are inside the url
                 final int abuseStart = currentUrl.indexOf("google_abuse=");
                 if (abuseStart != -1) {
                   final int abuseEnd = currentUrl.indexOf("+path");
                   try {
-                    String? abuseCookie =
-                        currentUrl.substring(abuseStart + 12, abuseEnd);
-                    abuseCookie = await (NewPipeExtractorDart.execute(
-                            'decodeCookie', {"cookie": abuseCookie})
-                        as FutureOr<String>);
+                    String? abuseCookie = currentUrl.substring(abuseStart + 12, abuseEnd);
+                    abuseCookie = await (NewPipeExtractorDart.execute('decodeCookie', {"cookie": abuseCookie}) as FutureOr<String>);
                     handleCookies(abuseCookie);
                   } catch (_) {}
                 }
-                await NewPipeExtractorDart.execute(
-                    'setCookie', {"cookie": foundCookies});
+                await NewPipeExtractorDart.execute('setCookie', {"cookie": foundCookies});
                 Navigator.pop(context);
               },
             ),
           ],
         ),
-        body: InAppWebView(
-          initialUrlRequest: URLRequest(
-              url: Uri.parse(url), headers: ExtractorHttpClient.defaultHeaders),
-          onLoadStop: (cont, _) {
-            controller = cont;
-          },
+        body: WebViewWidget(
+          controller: controller,
         ),
       ),
     );
@@ -98,10 +98,7 @@ class _ReCaptchaPageState extends State<ReCaptchaPage> {
     if (cookies == null) {
       return;
     }
-    if (cookies.contains("s_gl=") ||
-        cookies.contains("goojf=") ||
-        cookies.contains("VISITOR_INFO1_LIVE=") ||
-        cookies.contains("GOOGLE_ABUSE_EXEMPTION=")) {
+    if (cookies.contains("s_gl=") || cookies.contains("goojf=") || cookies.contains("VISITOR_INFO1_LIVE=") || cookies.contains("GOOGLE_ABUSE_EXEMPTION=")) {
       if (foundCookies.contains(cookies)) {
         return;
       }
